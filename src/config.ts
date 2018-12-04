b8b417d687b123c7b1f92933f0efbfa5e52169b6
Aktyn <Aktyn3@gmail.com>
2018-12-05 00:23:30 +0100

    quick fix

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,8 @@
 export default {
-	api_server_url: 'http://localhost:1234',
+	api_server_url: process.env.NODE_ENV === 'development' ? 
+		'http://localhost:1234' : 
+		'http://54.37.128.46',
 	short_description: 'TODO - krotki opis',
 	long_description: 'TODO - dlugi opis'
 };
+console.log();
