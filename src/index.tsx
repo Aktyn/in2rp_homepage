ec2758837a97e607bb9e8f528b98283ef92884d7
Aktyn <Aktyn3@gmail.com>
2019-01-06 01:07:03 +0100

    fix attempt

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Config from './config';
+/*import Config from './config';
 if(process.env.NODE_ENV !== 'development' && 
 	location.hostname.replace(/^www\./i, '') !== Config.hostname) 
 {
 	location.hostname = Config.hostname;
-}
+}*/
 
 // import './styles/normalize.css';
 import './styles/main.scss';
